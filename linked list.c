570b464a884f9cd7dd3c92e40fce7a8acc791ce0
chaeyoung <456codud@naver.com>
2018-12-06 15:40:44 +0900

    3

--- linked list.c
+++ linked list.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct linknd{
 	int data;
@@ -5,3 +7,21 @@ typedef struct linknd{
 }linknd_t;
 
 static linknd_t *list;
+
+linknd_t* create_node(int value){
+	linknd_t* ndptr;
+	
+	ndptr = (linknd_t*)malloc(sizeof(linknd_t));
+	if(ndptr == NULL)
+	{
+		printf("ERROR \n");
+		return NULL;
+	}
+	
+	ndptr->data = value;
+	ndptr->next = NULL;
+	
+	
+	return ndptr;
+}
+
